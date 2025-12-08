760d4c0b44290410a7e53049f08a474c93f0fdca
Sadhna Kumari <ss.sadhna263@gmail.com>
2025-12-09 03:05:07 +0530

    filters

--- frontend/src/components/ui/filters.jsx
+++ frontend/src/components/ui/filters.jsx
@@ -1,24 +1,20 @@
-import Autocomplete from '@mui/material/Autocomplete';
-import TextField from '@mui/material/TextField';
-import { useCustomFilterOptions } from './CustomFilter';
 import { ChevronDown } from 'lucide-react';
 
-export const Filters = ({ options = [] }) => {
+export const Filters = ({ values = [] }) => {
     return (
         <div className="flex gap-4 items-center">
             <div className="relative">
                 <select className="custom-select">
-                    <option>Region</option>
-                    <option>Gurgaon</option>
-                    <option>Delhi</option>
-                    <option>Greater Noida</option>
+                    {values.map((value, idx)=>(
+                        <option key={idx}>{value}</option>
+                    ))}
                 </select>
                 <div className="pointer-events-none absolute right-3 top-1/2 -translate-y-1/2 text-gray-500">
                     <ChevronDown size={20} color="black" />
                 </div>
             </div>
 
-            <div className="relative">
+            {/* <div className="relative">
                 <select className="custom-select">
                     <option>Year</option>
                     <option>2025</option>
@@ -39,7 +35,7 @@ export const Filters = ({ options = [] }) => {
                 <div className="pointer-events-none absolute right-3 top-1/2 -translate-y-1/2 text-gray-500">
                     <ChevronDown size={20} color="black" />
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
